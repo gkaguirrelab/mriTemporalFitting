4d7c5bc932f0931236b02d0517c5286adefcff89
gkaguirre <gkaguirre@me.com>
2018-07-17 12:14:41 -0400

    Building a tutorial and forward model
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- Models/RPRF/forwardModelRPRF/forwardModelRPRF.m
+++ Models/RPRF/forwardModelRPRF/forwardModelRPRF.m
@@ -10,56 +10,19 @@ function [modelResponseStruct] = forwardModelRPRF(obj,params,stimulusStruct, hrf
 % this kernel.
 %
 
-amplitudeVec=params.paramMainMatrix(:,strcmp(params.paramNameCell,'amplitude'));
-durationVec=params.paramMainMatrix(:,strcmp(params.paramNameCell,'duration'));
+xPos=params.paramMainMatrix(:,strcmp(params.paramNameCell,'xPos'));
+yPos=params.paramMainMatrix(:,strcmp(params.paramNameCell,'yPos'));
 
-%% Define basic model features
 
-% derive some basic properties of the stimulus values
-numInstances=size(stimulusStruct.values,1);
-modelLength = length(stimulusStruct.timebase);
-
-% pre-allocate the responseMatrix variable here for speed
-responseMatrix=zeros(numInstances,modelLength);
-
-% determine the deltaT of the stimulus and make sure it is regularly
-% sampled (in msecs)
-check = diff(stimulusStruct.timebase);
-deltaT = check(1);
-if (any(abs(check - check(1)) > 1e-6))
-    error('Response structure timebase is not regularly sampled');
-end
-
-%% We loop through each column of the stimulus matrix
-for i=1:numInstances
-    
-    % grab the current stimulus
-    subStimulusStruct=stimulusStruct;
-    subStimulusStruct.values=subStimulusStruct.values(i,:);
-
-    % Build the kernel
-    stepKernelStruct.timebase=0:deltaT:max([ceil(durationVec(i)*1000-deltaT) deltaT]);
-    stepKernelStruct.values=stepKernelStruct.timebase*0+1;
-    
-    % Convolve the stimulus by the step kernel
-    yNeuralStruct=obj.applyKernel(subStimulusStruct,stepKernelStruct);
+% Obtain the temporal profile of the stimulus at the x, y location
+    modelResponseStruct.timebase=stimulusStruct.timebase;
+    modelResponseStruct.values=squeeze(stimulusStruct.values(xPos,yPos,:));
 
     % Convolve the stimulus by the hrf kernel
-    yNeuralStruct=obj.applyKernel(yNeuralStruct,hrfKernelStruct);
+    modelResponseStruct=obj.applyKernel(modelResponseStruct,hrfKernelStruct);
 
     % make the response have unit amplitude
-    yNeuralStruct.values=yNeuralStruct.values./max(yNeuralStruct.values);
+    modelResponseStruct.values=yBOLDStruct.values./max(yBOLDStruct.values);
 
-    % Scale by the amplitude parameter
-    yNeuralStruct.values=yNeuralStruct.values.*amplitudeVec(i);
-    
-    %% Place yNeural into the growing neuralMatrix
-    responseMatrix(i,:)=yNeuralStruct.values';
-    
-end % loop over rows of the stimulus matrix
-
-%% Build the modelResponseStruct to return
-modelResponseStruct.timebase=stimulusStruct.timebase;
-modelResponseStruct.values=sum(responseMatrix,1);
 
 end
---
+++ Tutorials/Models/RPRF/t_RPRFBasic.m
@@ -0,0 +1,91 @@
+function validationData = t_RPRFBasic(varargin)
+% validationData = t_RPRFBasic(varargin)
+%
+% Demonstrate pRF model
+%
+% We will model a single event.
+%
+% Optional key/value pairs
+%  'generatePlots' - true/fale (default true).  Make plots?
+
+%% Parse vargin for options passed here
+p = inputParser;
+p.addParameter('generatePlots',true,@islogical);
+p.parse(varargin{:});
+
+%% Construct the model object
+tfeHandle = tfeRPRF('verbosity','none');
+
+%% Spatial and Temporal definition of the stimulus
+deltaT = 3000; % in msecs
+totalTime = 300000; % in msecs
+xSize = 10;
+ySize = 10;
+stimulusStruct.timebase = linspace(0,totalTime-deltaT,totalTime/deltaT);
+nTimeSamples = size(stimulusStruct.timebase,2);
+
+% Create a stimulus movie (xSize x ySize x time)
+nInstances=1;
+defaultParamsInfo.nInstances=nInstances;
+stimulusStruct.values=zeros(xSize,ySize,nTimeSamples);
+
+% Have a stimulus bar drift upwards over the time
+for tt = 1:nTimeSamples
+    xPos = mod(tt,xSize);
+    stimulusStruct.values(xPos,:,tt)=1;
+end
+
+%% Define a kernelStruct. In this case, a double gamma HRF
+hrfParams.gamma1 = 6;   % positive gamma parameter (roughly, time-to-peak in secs)
+hrfParams.gamma2 = 12;  % negative gamma parameter (roughly, time-to-peak in secs)
+hrfParams.gammaScale = 10; % scaling factor between the positive and negative gamma componenets
+
+deltaT = 3000; % in msecs
+kernelStruct.timebase = linspace(0,totalTime-deltaT,totalTime/deltaT);
+
+% The timebase is converted to seconds within the function, as the gamma
+% parameters are defined in seconds.
+hrf = gampdf(kernelStruct.timebase/1000, hrfParams.gamma1, 1) - ...
+    gampdf(kernelStruct.timebase/1000, hrfParams.gamma2, 1)/hrfParams.gammaScale;
+kernelStruct.values=hrf;
+
+% prepare this kernelStruct for use in convolution as a BOLD HRF
+kernelStruct.values=kernelStruct.values-kernelStruct.values(1);
+kernelStruct=normalizeKernelArea(kernelStruct);
+
+% Get the default forward model parameters
+params0 = tfeHandle.defaultParams('defaultParamsInfo', defaultParamsInfo);
+params0.noiseSd = 0.01;
+
+% start the packet assembly
+thePacket.stimulus = stimulusStruct;
+thePacket.kernel = kernelStruct;
+thePacket.metaData = [];
+
+% Randomize the order of the stimTypes
+thePacket.stimulus.metaData.stimTypes=[1];
+thePacket.stimulus.metaData.stimLabels=['demo'];
+
+% Create some params to define the simulated data for this packet
+paramsLocal=params0;
+paramsLocal.paramMainMatrix(1,1)=1;
+paramsLocal.paramMainMatrix(1,2)=4; % xPos
+paramsLocal.paramMainMatrix(1,3)=5; % yPos
+
+%% Report the modeled params
+fprintf('Simulated model parameters:\n');
+tfeHandle.paramPrint(paramsLocal);
+fprintf('\n');
+
+% Generate the simulated response
+simulatedResponseStruct = tfeHandle.computeResponse(paramsLocal,thePacket.stimulus,thePacket.kernel,'AddNoise',true);
+
+% Add the simulated response to this packet
+thePacket.response=simulatedResponseStruct;
+
+if p.Results.generatePlots
+    tfeHandle.plot(simulatedResponseStruct,'DisplayName','Simulated');
+end
+
+
+end % function
